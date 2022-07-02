b4c99c47b48562f5318aa892055be1f561d566c3
hegugu-ng <2949434209@qq.com>
2022-07-02 18:20:56 +0800

    新增读秒，优化轮询方案，适配beforeUnmount

--- src/components/ng/DataGroup.vue
+++ src/components/ng/DataGroup.vue
@@ -20,7 +20,7 @@ export default {
   border: 1px solid #ebeef5;  
   border-radius: 10px;
   /* padding: 20px; */
-  box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+  box-shadow: 0 2px 22px 0 rgb(0 0 0 / 10%);
   color: #242f57;
 }
 .ng-info-card > .title {
