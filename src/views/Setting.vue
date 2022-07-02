4524bbd266650f91975e774cdeee289435a46e8e
hegugu-ng <2949434209@qq.com>
2022-07-02 20:30:34 +0800

    系统设置页面

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,3 +1,23 @@
 <template>
-    <div class="setting">系统设置</div>
+    <div class="setting">
+        <ng-infocard title="系统设置" update="">
+            <ng-datagroup :CardItem="CoreData"></ng-datagroup>
+        </ng-infocard>
+    </div>
 </template>
+
+<script>
+import InfoCard from "../components/ng/InfoCard";
+
+export default {
+  components: {
+    "ng-infocard": InfoCard,
+  },
+}
+</script>
+
+<style>
+.setting {
+    padding: 10px;
+}
+</style>
