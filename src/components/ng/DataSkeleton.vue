a04ae56c91ac1ef047ea3f1becadbc99bdff5af6
hegugu-ng <2949434209@qq.com>
2023-01-08 00:12:25 +0800

    修改了核心数据骨架屏错误的高度，避免了高度闪烁

--- src/components/ng/DataSkeleton.vue
+++ src/components/ng/DataSkeleton.vue
@@ -4,7 +4,7 @@
       <template #template>
         <div style="border: 1px solid #e9eaec; border-radius: 8px; background: #fff; padding: 10px">
           <el-skeleton-item variant="text" style="font-size: 15px; width: 40%" />
-          <el-skeleton-item variant="rect" style="margin-top: 12px; height: 30px; width: 55%; display: block" />
+          <el-skeleton-item variant="rect" style="margin-top: 16px; height: 30px; width: 55%; display: block" />
         </div>
       </template>
     </el-skeleton>
